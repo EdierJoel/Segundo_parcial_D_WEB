fb7e82b2aa316d85dbcb7be62ff066e444f992ad
EdierJoelCabGonzalez <46614287+EdierJoelCabGonzalez@users.noreply.github.com>
2019-02-17 22:11:08 -0500

    Se realizo la validación del login, que si se cumplieran ciertas sentencias imprimiera alertas.

--- backend/includes/_funciones.php
+++ backend/includes/_funciones.php
@@ -1,42 +1,52 @@
 ﻿<?php  
 	require_once("_db.php");
-	
-switch ($_POST["accion"]) {
-		
-case 'login':
+    switch ($_POST["action"]) {
+    
+    case 'login':
 			
-login();
+    login();
 			
-break;
+    break;
 		
 		
-default:
+    default:
 
-# code...
+    # code...
 
-break;
+    break;
 	
 }
 	
-function login(){
+      function login(){
+		global $mysqli;
+		// Conectar a Base de Datos.
+		$correo = $_POST["correo"];
+		$pass = $_POST["password"];	
+		// Consultar a Base de Datos que exista el usuario.
+		$consulta = "SELECT * FROM usuarios WHERE correo_usr = '$correo'";
+		$resultado = $mysqli->query($consulta);
+		$fila = $resultado->fetch_assoc();
 		
-     // echo "Tu usuario es: ".$_POST["usuario"].", Tu contraseña es: ".$_POST["password"];
+		if ($fila == 0) 
+			{
+				// 	Si el usuario no existe imprimir = 2
+				echo "Usuario no existe Error 02";
 
-    // Conectar a Base de Datos.
+			}
 
-	// 	Si usuario y contraseña estan vacios imprimir = 3
- 
-	// Consultar a Base de Datos que exista el usuario.
-		
-    // Si el usuario existe, consultar que el password sea correcto. 
-
-	// Si el password es correcto imprimir = 1 
-
-	// Si el password no es correcto, imprimir codigo de erorres = 0.
-
-    // Si el usuario no existe imprimir = 2
-	
-}
-
-
-?>
+			// 	Si el usuario existe, conusltar que el password sea correcto. 
+		else if ($fila["password"] != $pass) 
+			{
+				$consulta = "SELECT * FROM usuarios WHERE correo_usr = '$correo' AND password = '$pass'";
+				$resultado = $mysqli->query($consulta);
+				$fila = $resultado->fetch_assoc();
+				// 			Si el password no es correcto, imprimir codigo de error 0.
+				echo "Password es Incorrecto Error 0";	
+			}
+				else if($correo == $fila["correo_usr"] && $pass == $fila["password"])
+				{
+					// 			Si el password es correcto imprimir = 1 
+					echo "Usuario y Password son Correctos Acceso 01"	;
+					}
+			}
+?>		
